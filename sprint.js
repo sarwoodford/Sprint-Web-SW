8b8bef9d50b508af02893a16ed07cdab36fb2477
sara woodford <157317240+sarwoodford@users.noreply.github.com>
2024-03-29 00:50:51 -0230

    Update sprint.js
    read the JSON file and called a function (readReport) which will contain my for each loop

--- sprint.js
+++ sprint.js
@@ -2,3 +2,12 @@
 // Decription: to create, read and display a JSON file
 // Author: Sara Woodford
 // Date(s) : March 28, 2024 - 
+
+fetch('sprint.json')
+  .then(response => response.json())
+  .then(report => {
+    console.log(report);
+    readReport(report);
+  })
+
+    
